a72668b90c9f31baa159cf9440f783d49fa0349a
ststeiger <quandary82@hailmail.net>
2015-07-30 14:18:38 +0200

    GeoData Switzerland

--- JavaScriptComponents/ajax/Data.ashx.cs
+++ JavaScriptComponents/ajax/Data.ashx.cs
@@ -9,12 +9,53 @@ namespace JavaScriptComponents.ajax
 {
 
 
+
     /// <summary>
     /// Zusammenfassungsbeschreibung für Data
     /// </summary>
     public class Data : IHttpHandler
     {
 
+
+        public static string StreamToString(System.IO.Stream stream)
+        {
+            stream.Position = 0;
+            using (System.IO.StreamReader reader = new System.IO.StreamReader(stream, System.Text.Encoding.UTF8))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+
+        public class Municipality
+        {
+            public int id;
+            public string name;
+        }
+
+
+        public static void SaveData(Municipality[] municipalitites)
+        {
+            System.Data.DataRow dr = null;
+            string strSQL = string.Format("SELECT * FROM {0} WHERE (1=2) ", "__Gemeinden");
+
+            using (System.Data.DataTable dt = SQL.GetDataTable(strSQL))
+            {
+                foreach (Municipality municipalitiy in municipalitites)
+                {
+                    dr = dt.NewRow();
+
+                    dr["MUN_Id"] = municipalitiy.id;
+                    dr["MUN_Name"] = municipalitiy.name;
+                    
+                    dt.Rows.Add(dr);
+                } // Next file
+
+                SQL.UpdateDataTable(dt, strSQL);
+                System.Console.WriteLine(dt.Rows.Count); // 34'409
+            } // End Using dt
+        }
+        
 
         public void ProcessRequest(HttpContext context)
         {
@@ -24,6 +65,15 @@ namespace JavaScriptComponents.ajax
             // https://www.nuget.org/packages/BingGeocodingHelper/
             // http://acconuts.codeplex.com/
             // http://www.codeproject.com/Articles/262179/SVG-World-Map
+
+            if (StringComparer.InvariantCultureIgnoreCase.Equals(System.Web.HttpContext.Current.Request.HttpMethod, "POST"))
+            {
+                string strJSON = StreamToString(System.Web.HttpContext.Current.Request.InputStream);
+                System.Console.WriteLine(strJSON);
+                Municipality[] municipalitites = Tools.JsonHelper.Deserialize<Municipality[]>(strJSON);
+                SaveData(municipalitites);
+                System.Console.WriteLine(municipalitites);
+            }
 
             context.Response.Write("Hello World");
         }
---
+++ JavaScriptComponents/switzerland/README.md
@@ -0,0 +1,1 @@
+Map of Switzerland with [TopoJSON](https://github.com/mbostock/topojson) and map data from [Swiss Maps](https://github.com/interactivethings/swiss-maps). Cantons and municipalities are combined in a single TopoJSON file.
